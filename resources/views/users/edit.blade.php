28f4ac5434171a04149786d718ab277bcb006581
Don Balbieran <dcbalbieranjr@lifeacademy.edu.ph>
2025-09-15 23:45:12 +0800

    frontdesk staff role

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -86,6 +86,7 @@
                             <option value="pnc.staff">P&C Staff</option>
                             <option value="pnc.admin">P&C Admin</option>
                             <option value="sys.admin">System Admin</option>
+                            <option value="frontdesk.staff">Front Desk</option>
                         </select>
                         <flux:button type="button" size="sm" variant="primary" onclick="addRole()">Add Role</flux:button>
                     </div>
