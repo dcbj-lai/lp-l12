48b9501e3efec027c0bc89a8f463d44686c63437
Don Balbieran <dcbalbieranjr@lifeacademy.edu.ph>
2026-05-02 03:36:04 +0800

    permission seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Always clear cache before seeding
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permissions = [
+            'leave.approve',
+            'leave.delete',
+
+            'reservation.approve',
+
+            'users.edit',
+            'users.authorize',
+            'users.list',
+            'users.compensation.manage',
+
+            'leave-credits.view',
+            'leave-credits.update',
+            'leave-credits.assign',
+
+            'attendance.view',
+            'attendance.create',
+            'attendance.update',
+            'attendance.manage',
+
+            'requests.hr.view',
+            'requests.hr.view.detail',
+            'requests.hr.purge',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(
+                ['name' => $permission, 'guard_name' => 'web'],
+                ['created_at' => now(), 'updated_at' => now()]
+            );
+        }
+
+        // Clear cache again after seeding
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+    }
+}
