65ffc0942b8eeff02ce49ac7354697c2f3c10419
Don Balbieran <dcbalbieranjr@lifeacademy.edu.ph>
2026-05-04 10:34:45 +0800

    remove redundant header in resource reservations

--- resources/views/livewire/resources/reservation-index.blade.php
+++ resources/views/livewire/resources/reservation-index.blade.php
@@ -20,17 +20,6 @@
         }
     }
 }" class="space-y-4">
-
-    <!-- Header -->
-    <div>
-        <h1 class="text-xl md:text-2xl font-semibold text-zinc-800 dark:text-zinc-100">
-            Resource Reservations
-        </h1>
-        <p class="text-sm text-gray-500">
-            Approval dashboard
-        </p>
-    </div>
-
     <!-- List -->
     <div class="space-y-3">
         @php
